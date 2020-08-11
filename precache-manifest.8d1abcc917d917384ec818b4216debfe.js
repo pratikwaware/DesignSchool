5b0a177db237523625b2bc6d63920344043836df
pratikwaware <pratikwaware919@gmail.com>
2020-08-11 17:24:36 +0530

    Updates

---
+++ precache-manifest.8d1abcc917d917384ec818b4216debfe.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbaf6c93b301456c1e2",
+    "url": "/DesignSchool/static/js/main.efbaf6c9.chunk.js"
+  },
+  {
+    "revision": "a4807cb20d88dd0130e2",
+    "url": "/DesignSchool/static/js/1.a4807cb2.chunk.js"
+  },
+  {
+    "revision": "0e02ff27d0006db80a18",
+    "url": "/DesignSchool/static/js/runtime~main.0e02ff27.js"
+  },
+  {
+    "revision": "dd32f6389a6441eabf75c6039a911848",
+    "url": "/DesignSchool/static/media/MaskGroup2.dd32f638.jpg"
+  },
+  {
+    "revision": "0ee69b185994f560b24dac6331a6f55b",
+    "url": "/DesignSchool/static/media/john-jennings.0ee69b18.jpg"
+  },
+  {
+    "revision": "cee0e63af2fd63db784eae3eaf048f1c",
+    "url": "/DesignSchool/static/media/jean-philippe-delberghe.cee0e63a.jpg"
+  },
+  {
+    "revision": "8adbc707dd13e9be040df960add9435a",
+    "url": "/DesignSchool/static/media/yellow.8adbc707.jpg"
+  },
+  {
+    "revision": "061ab0b04fdcc6758404a554891a81b1",
+    "url": "/DesignSchool/static/media/blue.061ab0b0.jpg"
+  },
+  {
+    "revision": "97f4e2cbb5ac0d1ffec8e718f2c48435",
+    "url": "/DesignSchool/static/media/purple.97f4e2cb.jpg"
+  },
+  {
+    "revision": "31a1dd23b821939df9f1865109ccb616",
+    "url": "/DesignSchool/static/media/MaskGroup2@2x.31a1dd23.jpg"
+  },
+  {
+    "revision": "e29e87d7a55d844d82b5c4383b25e7e2",
+    "url": "/DesignSchool/static/media/blue@2x.e29e87d7.jpg"
+  },
+  {
+    "revision": "74c617fc4a607c31c26aebdcd787d47d",
+    "url": "/DesignSchool/static/media/meteor-dribbble-1.74c617fc.jpg"
+  },
+  {
+    "revision": "c7234d75b7073bd1234ab97bdc4960c6",
+    "url": "/DesignSchool/static/media/meteor-dribbble-2.c7234d75.jpg"
+  },
+  {
+    "revision": "3c2b21f5982aea473e506494d9c22f12",
+    "url": "/DesignSchool/static/media/meteor-dribbble-3.3c2b21f5.jpg"
+  },
+  {
+    "revision": "8f2d39fdc9a790f246b5e03015ad53ba",
+    "url": "/DesignSchool/static/media/meteor-dribbble-4.8f2d39fd.jpg"
+  },
+  {
+    "revision": "0b192f15aa7b2fa4927ec24387bdfdb3",
+    "url": "/DesignSchool/static/media/meteor-dribbble-5.0b192f15.jpg"
+  },
+  {
+    "revision": "05da47ebdbb4b78210c8170c913ababc",
+    "url": "/DesignSchool/static/media/meteor-dribbble.05da47eb.jpg"
+  },
+  {
+    "revision": "7e1de4bf0cd18de5a44c4cb4760040db",
+    "url": "/DesignSchool/static/media/purple@2x.7e1de4bf.jpg"
+  },
+  {
+    "revision": "597f57fe42efdcf3389f4ee4ad5576f8",
+    "url": "/DesignSchool/static/media/undraw_teacher.597f57fe.svg"
+  },
+  {
+    "revision": "4238e6fe9f00bb76bc45494962f3070c",
+    "url": "/DesignSchool/static/media/yellow@2x.4238e6fe.jpg"
+  },
+  {
+    "revision": "9cc230f8ca2e0d56ea6acc4a836c57f4",
+    "url": "/DesignSchool/index.html"
+  }
+];
